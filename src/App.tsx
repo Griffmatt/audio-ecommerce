6fd286d178471c1a22f03d3e6d1d72f98416ad51
Griffin <griffinmmatthews@gmail.com>
2022-06-26 18:19:43 -0400

    added nav bar

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./CSS/App.css";
+import "./CSS/Variables.css";
+import "./CSS/NavBar.css";
+
+import NavBar from "./Components/NavBar";
+
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+    <div className="appWrapper">
+       
+    
+    </div>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div className="navBar">
+        <div className="navWrapper">
+            <img src="/assets/shared/desktop/logo.svg"/>
+            <ul>
+                <li>HOME</li>
+                <li>HEADPHONES</li>
+                <li>SPEAKERS</li>
+                <li>EARPHONES</li>
+            </ul>
+            <img/>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
