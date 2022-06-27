c045d08eac8c2863f2ddff939740379adeedfca2
Griffin <griffinmmatthews@gmail.com>
2022-06-27 17:35:28 -0400

    background image rework

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 function Banner() {
   return (
     <div className="banner">
-        <div className="componentWrapper">
-            <div className="bannerContent">
-                <div className="overLine">NEW PRODUCT</div>
-                <h1>XX99 MARK II<br/>HEADPHONES</h1>
-                <p>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
-                <button className="buttonOne subTitle">SEE PRODUCT</button>
+        <div className="bannerBackground">
+            <div className="componentWrapper">
+                <div className="bannerContent">
+                    <div className="overLine">NEW PRODUCT</div>
+                    <h1>XX99 MARK II<br/>HEADPHONES</h1>
+                    <p>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+                    <button className="buttonOne subTitle">SEE PRODUCT</button>
+                </div>
             </div>
         </div>
     </div>
